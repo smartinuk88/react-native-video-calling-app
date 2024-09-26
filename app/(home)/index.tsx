8d0f294a7053737097681c7d515615bddb1b633e
Scott Martin <scott.martin88@live.com>
2024-09-26 15:03:41 +0200

    Implement sign out functionality

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,23 +1,41 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { SignedIn, useAuth, useUser } from "@clerk/clerk-expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import Dialog from "react-native-dialog";
 
 export default function Page() {
+  const [dialogOpen, setDialogOpen] = useState(false);
   const { user } = useUser();
+  const { signOut } = useAuth();
 
   return (
     <View>
+      <TouchableOpacity
+        style={{ position: "absolute", top: 20, right: 20, zIndex: 100 }}
+        onPress={() => setDialogOpen(true)}
+      >
+        <MaterialCommunityIcons name="exit-run" size={24} color="#5F5DEC" />
+      </TouchableOpacity>
+
+      <Dialog.Container visible={dialogOpen}>
+        <Dialog.Title>Sign Out</Dialog.Title>
+        <Dialog.Description>
+          Are you sure you want to sign out?
+        </Dialog.Description>
+        <Dialog.Button label="Cancel" onPress={() => setDialogOpen(false)} />
+        <Dialog.Button
+          label="Sign Out"
+          onPress={async () => {
+            signOut();
+            setDialogOpen(false);
+          }}
+        />
+      </Dialog.Container>
+
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
-      <SignedOut>
-        <Link href="/sign-in">
-          <Text>Sign In</Text>
-        </Link>
-        <Link href="/sign-up">
-          <Text>Sign Up</Text>
-        </Link>
-      </SignedOut>
     </View>
   );
 }
